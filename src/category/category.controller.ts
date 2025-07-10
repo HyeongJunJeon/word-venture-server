b293dcbf8782502957a2a3660ae10e6d6fc3658c
HyeongJunJeon <wehj0105@gmail.com>
2025-07-10 17:03:13 +0900

    Feat: 앤드포인트 별 summary 추가

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -15,21 +16,25 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 export class CategoryController {
   constructor(private readonly CategoryService: CategoryService) {}
 
+  @ApiOperation({ summary: '카테고리 생성' })
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.CategoryService.create(createCategoryDto);
   }
 
+  @ApiOperation({ summary: '모든 카테고리 조회' })
   @Get()
   findAll() {
     return this.CategoryService.findAll();
   }
 
+  @ApiOperation({ summary: '카테고리 상세 조회' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.CategoryService.findOne(+id);
   }
 
+  @ApiOperation({ summary: '카테고리 수정' })
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +43,7 @@ export class CategoryController {
     return this.CategoryService.update(+id, UpdateCategoryDto);
   }
 
+  @ApiOperation({ summary: '카테고리 삭제' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.CategoryService.remove(+id);
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,42 +1,51 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
   Query,
 } from '@nestjs/common';
-import { PostService } from './post.service';
+import { ApiOperation } from '@nestjs/swagger';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
 import { GetPostDto } from './dto/get-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
+  @ApiOperation({ summary: '포스트 생성' })
   @Post()
   create(@Body() createPostDto: CreatePostDto) {
     return this.postService.create(createPostDto);
   }
 
+  @ApiOperation({
+    summary:
+      '모든 포스트 조회 (title 검색, level 필터링, category_id 필터링 가능)',
+  })
   @Get('')
   findAll(@Query() getPostDto: GetPostDto) {
     return this.postService.findAll(getPostDto);
   }
 
+  @ApiOperation({ summary: '포스트 상세 조회' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.postService.findOne(+id);
   }
 
+  @ApiOperation({ summary: '포스트 수정' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postService.update(+id, updatePostDto);
   }
 
+  @ApiOperation({ summary: '포스트의 질문 답변 수정' })
   @Patch('/question/:id')
   updateQuestionAnswer(
     @Param('id') id: string,
@@ -45,6 +54,7 @@ export class PostController {
     return this.postService.updateQuestionAnswer(+id, questionAnswer);
   }
 
+  @ApiOperation({ summary: '포스트 삭제' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.postService.remove(+id);
